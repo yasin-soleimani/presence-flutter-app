973cc5613f03567e60bfc3b1576475e35c23b7d5
yasin-soleimani <yasin.soleimani.dev@gmail.com>
2025-03-09 23:54:16 +0330

    start design landing page without core components

--- lib/features/landing/presentation/pages/landing_page.dart
+++ lib/features/landing/presentation/pages/landing_page.dart
@@ -1,60 +1,91 @@
-import 'package:flutter/material.dart';
 import 'package:app/features/auth/presentation/pages/login_page.dart';
 import 'package:app/features/auth/presentation/pages/register_page.dart';
-import 'package:app/core/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text(
+            'Landing Page',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              const Spacer(),
-              const SizedBox(height: 72),
-              // Welcome Text
-              Text(
-                'به Noteback خوش آمدید',
-                style: Theme.of(context).textTheme.headlineLarge,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 16),
-              Text(
-                'متن ساختگی',
-                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const Spacer(),
-              // Login Button
-              CustomButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginPage()),
-                  );
-                },
-                text: 'ورود',
-              ),
-              const SizedBox(height: 16),
-              // Register Button
-              CustomButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const RegisterPage(),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 250,
+                    width: 300,
+
+                    child: Text(
+                      'NoteBack App',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
-                  );
-                },
-                text: 'ثبت نام',
-                isOutlined: true,
+                  ),
+                  SizedBox(
+                    width: double.infinity, // Makes the button take full width
+                    height: 42,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      },
+                      child: Text('Login Button'),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  SizedBox(
+                    width: double.infinity, // Makes the button take full width
+                    height: 42,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RegisterPage(),
+                          ),
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          side: BorderSide(color: Colors.white, width: 4),
+                        ),
+                      ),
+                      child: Text(
+                        'Register Button',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 42),
             ],
           ),
         ),
